77854a899f5e9b9e6c35bfa6038d1f558573b9a4
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-10-24 14:44:11 +0100

    Update forms.py

--- HalloweenFun/HauntedCheese/forms.py
+++ HalloweenFun/HauntedCheese/forms.py
@@ -1,6 +1,6 @@
 
 from django import forms
-from django.contrib.auth.models import User, Todo
+from django.contrib.auth.models import User
 from django.forms import widgets
 from HauntedCheese.models import Todo
 
