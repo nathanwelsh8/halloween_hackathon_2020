4ab92ada9c141c5a1fd71edfcca8aa85eda9ba22
Liam Halpin <liamh2k@gmail.com>
2020-10-24 15:06:38 +0100

    fixed some stressy changes

--- HalloweenFun/HauntedCheese/forms.py
+++ HalloweenFun/HauntedCheese/forms.py
@@ -20,5 +20,5 @@ class AddItem(forms.ModelForm):
 
     title = forms.CharField(max_length=100, widget = forms.TextInput(attrs={'placeholder':"List Item Name", 'class':'form-control'}))
     description = forms.CharField(max_length=512, widget=forms.Textarea(attrs={'placeholder':'What spooky stuff do you need to do?', 'class':'form-control'}))  
-    start = forms.DateTimeField(input_formats=["%d %b %Y %H:%M:%S %Z"], widgets=forms.DateTimeInput(attrs={'class':'form-control'}))
-    due = forms.DateTimeField(input_formats=["%d %b %Y %H:%M:%S %Z"], widgets=forms.DateTimeInput(attrs={'class':'form-control'}))
+    #start = forms.DateTimeField(input_formats=["%d %b %Y %H:%M:%S %Z"], widgets=forms.DateTimeInput(attrs={'class':'form-control'}))
+    #due = forms.DateTimeField(input_formats=["%d %b %Y %H:%M:%S %Z"], widgets=forms.DateTimeInput(attrs={'class':'form-control'}))
