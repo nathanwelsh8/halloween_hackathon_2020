425021968b3be8b4ff72dc01ad9137a2e3e81a0b
Krishang Patney <2405690p@student.gla.ac.uk>
2020-10-24 13:01:12 +0100

    Add Stuff

--- HalloweenFun/HauntedCheese/urls.py
+++ HalloweenFun/HauntedCheese/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('createList/',views.ViewList.as_view(), name='viewList'),
     path('login/', views.Login.as_view(), name='login'),
-    path('register/', views.Register.as_view(), name='register')
+    path('register/', views.Register.as_view(), name='register'),
+    path('spook/', views.Spook.as_view(), name='spook')
+
 ]
