0a24e80f50a327f2dc89328e103e90bc90d60ed4
Liam Halpin <liamh2k@gmail.com>
2020-10-24 11:39:30 +0100

    added Todo and TodoIterator models to models.py and registered them on admin.py

--- HalloweenFun/HauntedCheese/admin.py
+++ HalloweenFun/HauntedCheese/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from models import Todo, TodoIterator
 
 # Register your models here.
+admin.site.register(TodoIterator)
+admin.site.register(Todo)
--- HalloweenFun/HauntedCheese/models.py
+++ HalloweenFun/HauntedCheese/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import timezone
 
-# Create your models here.
+PENDING = 0
+DONE = 1
+
+COMPLETE_STATUS = (
+    (PENDING, 'Pending'),
+    (DONE, 'Done')
+)
+
+class TodoIterator(models.Model):
+    list_names = models.CharField(max_length=255)
+
+# Todo List Model
+class Todo(models.Model):
+    list_title = models.ForeignKey(TodoIterator, on_delete=models.CASCADE)
+    list_content = models.TextField(blank=True)
+    created_time = models.DateTimeField(default=timezone.now())
+    due_time = models.DateTimeField(default=timezone.now())
+    status = models.PositiveSmallIntegerField(choices=COMPLETE_STATUS)
+
+    class Meta:
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return self.list_title
