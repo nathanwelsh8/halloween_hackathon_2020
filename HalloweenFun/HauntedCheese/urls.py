6fe6405881539be781474e1f4ecd73dbd8ad4486
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-10-24 12:05:38 +0100

    Added Views for login and register

--- HalloweenFun/HauntedCheese/urls.py
+++ HalloweenFun/HauntedCheese/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('createList/',views.ViewList.as_view(), name='viewList'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('register/', views.Register.as_view(), name='register')
 ]
--- HalloweenFun/HauntedCheese/views.py
+++ HalloweenFun/HauntedCheese/views.py
@@ -1,14 +1,16 @@
 from django.http import request
 from django.shortcuts import render
+from django.views import View
 
-  
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
-
-from HauntedCheese.models import Todo, TodoIterator
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 from django.urls import reverse 
 
-from django.views import View
+from HauntedCheese.models import Todo, TodoIterator
+from HauntedCheese.forms import UserForm 
+
 
 # Create your views here.
 
@@ -33,3 +35,63 @@ class ViewList(View):
         pass
     def post(self, request):
         pass
+
+class Login(View):
+
+    context_dict = {}
+    def get(self, request, **kwargs):
+        pass
+
+    def post(self,request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # If they clicked the register button they are redirected to register page.
+        if 'register' in request.POST:
+            return redirect(reverse('spatulaApp:register'))
+
+        user = authenticate(username=str(username.lower()), password=str(password))
+        
+        if user: 
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('spatulaApp:index'))
+            else: 
+                return self.get(request, **{"login_error_msg":"Your Spatula account has been disabled."})
+                
+        else:
+            return self.get(request, **{"login_error_msg":"Invalid login details supplied."})
+
+class Register(View):
+    context_dict = {}
+
+    def get(self,request, **kwargs):
+        pass
+
+    def post(self,request):
+         # Attempt to grab information from the raw form information.
+        # Note that we make use of both UserForm
+        user_form = UserForm(request.POST)
+
+        if user_form.is_valid():
+            # Save the user's form data to the database.
+            user = user_form.save()
+            user.username = user.username.lower()
+            # Now we hash the password with the set_password method.
+            # Once hashed, we can update the user object.
+            user.set_password(user.password)
+            user.save()
+           
+            # Update our variable to indicate that the template
+            # registration was successful.
+            registered = True
+            
+            user = authenticate(username=user.username.lower(), password=user_form['password'].data)
+            if user:
+                login(request, user)
+                
+            return redirect(reverse('spatulaApp:index'))
+        else:
+            # Invalid form or forms - mistakes or something else?
+            # Print problems to the terminal.
+            return self.get(request,**{"errors":user_form.errors})
