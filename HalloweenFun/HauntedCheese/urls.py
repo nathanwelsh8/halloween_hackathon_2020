3123ed90c96620e22a9a1b3ef4a6bbf18a8425f1
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-10-24 11:21:51 +0100

    created index page mapping

--- HalloweenFun/HauntedCheese/urls.py
+++ HalloweenFun/HauntedCheese/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from HauntedCheese import views
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
 
 ]
--- HalloweenFun/HauntedCheese/views.py
+++ HalloweenFun/HauntedCheese/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+  
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+
+#from spatulaApp.models import Recipe, Category, RecipeImage, UserProfile, Rating, UserImage
+from django.urls import reverse 
+
+from django.views import View
+
 # Create your views here.
-def index(request):
-    return render(request, 'HauntedCheese/index.html')
+
+class Index(View):
+
+    def post(self, request):
+        pass
+    def get(self, request):
+        return render(request, 'HauntedCheese/index.html')
